2b668e496c00ec70b8ecad71dbc0e3c12ce9a09a
Burhan Aslam <burhanaslam944@gmail.com>
2025-04-02 23:23:18 +0500

    Updated code for Problem 04

--- Problem 04.cpp
+++ Problem 04.cpp
@@ -1,66 +1,25 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
 
-int findPosition(int* nums, int n, int val)
+int findPosition(int *arr, int n, int val)
 {
 
-        for(int i = 0;i<n;i++)
-        {
-        if(nums[i]==val){
-        return i;
-        }
-        else if(val<nums[0]){
-        return 0;
-        }
-        else if(val<nums[i]&&val>nums[i-1]){
-        return i;
-        }
-        else if(val>nums[n-1]){
-        return n;
-        }
-        
-        
-    // int left = 0, right = n-1, mid;
+    // cout << "I had modified this code, Now I am checking this only " << endl;
+    int left = 0, right = n - 1, mid;
 
-    // while(left <= right)
-    // {
-    //     int mid = left + (right - left) / 2;
-    //     if(arr[mid] == val) return mid;
-    //     else if(arr[mid] < val) left = mid + 1;
-    //     else right = mid - 1;
-    // }
-    // return left;
-
-    // int k = -1;
-    // bool found = false;
-    // for (int i = 0; i < n; ++i)
-    // {
-    //     if (arr[i] == val)
-    //     {
-    //         k = i;
-    //         found = true;
-    //         break;
-    //     }
-    // }
-
-    // if (!found)
-    // {
-    //     for (int i = 0; i < n; ++i)
-    //     {
-    //         if(i > 0 && (i != n-1) && arr[i+1] > val && arr[i-1] < val)
-    //         {
-    //             k = i;
-    //         }
-    //     }
-    //     if(k == -1)
-    //     {
-    //         k = n;
-    //     }
-    // }
-    // return k;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == val)
+            return mid;
+        else if (arr[mid] < val)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
 }
-
 
 int main()
 {
