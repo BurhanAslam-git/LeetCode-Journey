29b2c777592ad5636dd4fd30a7b1414dcf657ffc
Burhan Aslam <burhanaslam944@gmail.com>
2025-04-03 16:07:36 +0500

    New File Added

---
+++ Problem_07.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+// bool isUnique(int* ptr, int n)
+// {
+//     bool hasDuplicate = false;
+//     for(int i=0; i<n; ++i)
+//     {
+//         if(i != n-1)
+//         {
+//             int j = i+1;
+//             while(j < n)
+//             {
+//                 if(ptr[i] == ptr[j])
+//                 {
+//                     hasDuplicate = true;
+//                     return hasDuplicate;
+//                 }
+//                 ++j;
+//             }
+//         }
+
+//     }
+//     return hasDuplicate;
+// }
+
+
+
+
+int main()
+{
+    int n;
+    cout << "Enter the size : ";
+    cin >> n;
+    while (n <= 0)
+    {
+        cout << "Invalid \n Enter again ";
+        cin >> n;
+    }
+    int *ptr = new int[n];
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "Enter Element " << i + 1 << " : ";
+        cin >> *(ptr + i);
+    }
+    bool s = hasDuplicate   (ptr,n);
+    if(s)
+    {
+        cout << "True" << endl;
+    }
+    else{
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
